ce97ab9a8bb158f6ae9913de81c69d6d0846d575
Bar <kahani1@gmail.com>
2018-12-26 13:59:29 -0800

    Add GET /todos rout

--- server/server.js
+++ server/server.js
@@ -21,8 +21,19 @@ app.post('/todos', (req, res) => {
   })
 })
 
-app
+app.get('/todos', (req, res) => {
+  Todo.find().then(() => {
+    res.send({
+      todos
+    })
+  }, (e) => {
+    res.status(400).send(e)
+  })
+})
 
 app.listen(3000, () => {
   console.log('Starting on port 3000')
 })
+
+
+module.exports = {app}
